ef05150251c95ca722f00e942d20e5761e572085
Marcel Wagner <wagmarcel@web.de>
2020-12-30 02:24:59 +0100

    with checking for free slots

--- services-operator/beamservicesoperator.py
+++ services-operator/beamservicesoperator.py
@@ -20,7 +20,7 @@ MAX_RETRY = os.getenv("OISP_BEAMOPERATOR_RETRY") or 20
 
 @kopf.on.startup()
 def configure(settings: kopf.OperatorSettings, **_):
-    settings.posting.level = logging.DEBUG
+    settings.posting.level = logging.INFO
 
 @kopf.on.create("oisp.org", "v1", "beamservices")
 def create(body, patch, spec, **kwargs):
@@ -50,9 +50,9 @@ def monitoring(stopped, patch, logger, body, spec, status, **kwargs):
 
     # State checking
     # if not deployed and not deploying => deploying
-    # if deploying => wait
+    # if deploying => exit and wait
     # if deploying and deployed => should not happen/deploying handler died unfinished
-    # same with jobCreated and jobCreating
+    # same procedure with jobCreated and jobCreating
     if status.get('deployed') is None:
         kopf.info(body, reason="Status None", message="Object not created")
         raise Exception("Object not created")
@@ -88,15 +88,10 @@ def monitoring(stopped, patch, logger, body, spec, status, **kwargs):
             f"{FLINK_URL}/jobs/{status.get('jobId')}")
         status_code = job_status.status_code
         
-        print("status_code " + str(status_code), flush=True)
+        # 404 - job not found
+        # 200 - job found, but state can be FAILED
         if status_code == 404:
-            #errors = job_status.json().get("errors", [])
-            #if not isinstance(errors, list):
-            #    matching = f"Job {status.get('jobId')} not found" in errors
-            #else:
-            #    matching = [element for element in errors if (f"Job {status.get('jobId')} not found" in element)]
-            #if bool(matching):
-            kopf.info(body, reason="Job not found", message="Job not found, triggering redeploy.")
+            kopf.info(body, reason="Job monitor", message="Job not found, triggering redeploy.")
             patch.status['deployed'] = False
             patch.status['jobCreated'] = False
             return {"updatedOn": str(datetime.now())}
@@ -105,10 +100,11 @@ def monitoring(stopped, patch, logger, body, spec, status, **kwargs):
             if pipeline_state is not None:
                 patch.status['state'] = pipeline_state
             if pipeline_state == 'FAILED':
+                kopf.info(body, reason="Job monitor", message="Job failed, triggering redeploy.")
                 cancel_job(body, status)
                 delete_jar(status)
                 reset_status(patch)
-                patch.status['state'] = "RESTARTING"            
+                patch.status['state'] = "RESETTING BY OPERATOR"
     except requests.exceptions.RequestException as e:
             kopf.info(body, reason="monitor job", message="Exception while trying to query job state. Reason: " + str(e))
     return
@@ -129,7 +125,7 @@ def reset(old, new, status, patch, body, spec, retry, **kwargs):
 
 @kopf.on.field("oisp.org", "v1", "beamservices", field="status.jobCreating")
 def jobCreating(old, new, status, patch, body, spec, retry, **kwargs):
-    """Submit jobs to Flink when status.jobCreating is triggered."""
+    """Submit jobs to Flink when status.jobCreating is triggered by monitor."""
     kopf.info(body, reason="debugging", message="Handler enters jobCreating with status " + str(new))
     if retry > MAX_RETRY:
         kopf.info(body, reason="jobCreating", message="Handler reached maximum retries. Reset states.")
@@ -149,7 +145,7 @@ def jobCreating(old, new, status, patch, body, spec, retry, **kwargs):
 
 @kopf.on.field("oisp.org", "v1", "beamservices", field="status.deploying")
 def deploying(old, new, status, patch, body, spec, retry, **kwargs):
-    """Deploy files when triggerd by status.deploying."""
+    """Deploy files when triggered by monitor."""
     kopf.info(body, reason="debugging", message="Handler enters deploying with status " + str(new))
     if retry > MAX_RETRY:
         kopf.info(body, reason="deploying", message="Handler reached maximum retries. Reset states.")
@@ -165,13 +161,6 @@ def deploying(old, new, status, patch, body, spec, retry, **kwargs):
             jar_path = None
             patch.status['jarPath'] = None
         jar_path = fetch_jar(body, spec)
-        #elif not os.path.isfile(jar_path):
-        #    kopf.info(body, reason="JarFile", message="jar_path " + str(jar_path) + " does not exist. Resetting deploying. Will fix later.")
-        #    patch.status['jarPath'] = None
-        #    patch.status['jarId'] = None
-        #    patch.status['deployed'] = False
-        #    patch.status['deploying'] = False
-        #    return {"updatedOn": str(datetime.now())}
         if jar_path is not None:
             patch.status['jarPath'] = jar_path
             jar_id = deploy(body, spec, jar_path)
@@ -275,9 +264,10 @@ def build_args(args_dict, tokens):
 
 def create_job(body, spec, jar_id):
     kopf.info(body, reason="createJob Task", message="starting job with jar_id " + str(jar_id))
+    if check_free_slots(body) == 0:
+        raise kopf.TemporaryError("No free task slots for submission. Try later again.", delay=5)
     entry_class = spec["entryClass"]
     tokens = util.get_tokens(spec.get("tokens", []))
-    kopf.info(body, reason="Got tokens", message=str(tokens))
     args = build_args(spec["args"], tokens)
     kopf.info(body, reason="Args Parsed",
               message=args)
@@ -295,3 +285,12 @@ def create_job(body, spec, jar_id):
     job_id = response.json().get("jobid")
     kopf.info(body, reason="Job created", message=f"Job id: {job_id}")
     return job_id
+
+def check_free_slots(body):
+    try:
+        response = requests.get(f"{FLINK_URL}/overview")
+        if response.status_code == 200:
+            free_slots = response.json().get("slots-available")
+            return free_slots
+    except requests.exceptions.RequestException as e:
+            kopf.info(body, reason="jobmanager overview", message="Exception while trying to check cluster state. Reason: " + str(e))
