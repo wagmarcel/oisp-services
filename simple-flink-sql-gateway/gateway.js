f5d147e17edb5459c2bd6801acacd2b0fa1c52c4
Marcel <wagmarcel@web.de>
2021-05-24 01:48:48 +0200

    gateway submits SQL and received job-id

--- simple-flink-sql-gateway/gateway.js
+++ simple-flink-sql-gateway/gateway.js
@@ -1,3 +1,4 @@
+"use strict";
 const express = require('express');
 const { exec } = require("child_process");
 const uuid = require("uuid");
@@ -15,18 +16,36 @@ app.post('/v1/sessions/:session_id/statements', function (request, response) {
   //for now ignore session_id
   var body = request.body;
   if (body.statement === undefined) {
-    response.status(599);
-    response.send("No statement field in body");
+    response.status(500);
+    response.send("Wrong format! No statement field in body");
     return;
   }
   var id = uuid.v4();
   var filename = "/tmp/script_" + id + ".sql";
   fs.writeFileSync(filename, body.statement.toString());
-  logger.info("Wrote " + filename + " with content " + body.statement);
-  var command = flink_root + "/bin/sql-client.sh -l " + sql_plugins + "-f " + filename;
-  //exec('')
-
-  response.send('OK');
+  var command = flink_root + "/bin/sql-client.sh -l " + sql_plugins + " -f " + filename;
+  logger.debug("Now executing " + command);
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      response.status(500);
+      response.send("Error while executing sql-client: " + error);
+      return;
+    }
+    //find Job ID ind stdout, e.g.
+    //Job ID: e1ebb6b314c82b27cf81cbc812300d97
+    var regexp = /Job ID: ([0-9a-f]*)/i;
+    var found = stdout.match(regexp);
+    logger.debug("Server output: " + stdout);
+    if (found !== null && found !== undefined) {
+      var jobId = found[1];
+      logger.debug("jobId found:" + jobId);
+      response.status(200).send('{ "id": "' + jobId + '" }');
+    } else { // no JOB ID found, unsuccessful
+      response.status(500);
+      response.send("Not successfully submitted. No JOB ID found in server reply.");
+      return;
+    }
+  });
 });
 
 
