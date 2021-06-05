983a2a94560e889db4934257591b167f41cfb635
Marcel <wagmarcel@web.de>
2021-06-05 22:11:08 +0200

    replaced submission term by flink term deploying/deployment

--- services-operator/beamsqlstatementsetoperator.py
+++ services-operator/beamsqlstatementsetoperator.py
@@ -3,7 +3,7 @@ import time
 import os
 import requests
 from enum import Enum
-
+import flink_util
 
 namespace = os.environ["OISP_NAMESPACE"] or "oisp"
 FLINK_URL = os.getenv("OISP_FLINK_REST") or f"http://flink-jobmanager-rest.{namespace}:8081"
@@ -14,14 +14,14 @@ timer_backoff_temporary_failure_seconds = os.getenv("TIMER_BACKOFF_TEMPORARY_FAI
 
 class States(Enum):
     INITIALIZED = "INITIALIZED",
-    SUBMITTED = "SUBMITTED",
-    SUBMISSION_FAILURE = "SUBMISSION_FAILURE"
+    DEPLOYING = "DEPLOYING",
+    DEPLOYMENT_FAILURE = "DEPLOYMENT_FAILURE",
+    RUNNING = "RUNNING",
+    FAILED = "FAILED",
+    CANCELED = "CANCELED",
+    CANCELING = "CANCELING"
 
-#states = {
-#    "INITIALIZED": "INITIALIZED",
-#    "SUBMITTED": "SUBMITTED",
-#    "SUBMISSION_FAILURE": "SUBMISSION_FAILURE"
-#}
+
 JOB_ID = "job_id"
 STATE = "state"
 
@@ -50,25 +50,22 @@ def updates(beamsqltables: kopf.Index, stopped, patch, logger, body, spec, statu
         state: STATE
         job_id: string
     STATE can be
-        - INITIALIZED - job_id: None
-        - SUBMITTED - job_id: flink_id
-        - CREATED
-        - RUNNING - job_id: flink_id
-        - FAILED
-        - SUBMISSION_FAILURE
-        - CANCELLED
-        - DELETING - job_id: None
-    Transitions:
-        - undefined/INITIALIZED => SUBMITTED
-        - INITIALIZED => SUBMISSION_FAILURE
-        - SUBMISSION_FAILURE => SUBMITTED
-        - SUBMITTED => RUNNING
-        - SUBMITTED => FAILED
-        - SUBMITTED => CANCELLED
-        - FAILED => DELETING
-        - CANCELLED => DELETING
-        - DELETING => INITIALIZED
+        - INITIALIZED - resource is ready to deploy job. job_id: None
+        - DEPLOYING - resource has been deployed, job_id: flink_id
+        - RUNNING - resource is running job_id: flink_id
+        - FAILED - resource is in failed state
+        - DEPLOYMENT_FAILURE - something went wrong while operator tried to deploy (e.g. server returned 500)
+        - CANCELED - resource has been canceled
+        - CANCELING - resource is in cancelling process
     
+    Transitions:
+        - undefined/INITIALIZED => DEPLOYING
+        - INITIALIZED => DEPLOYMENT_FAILURE
+        - DEPLOYMENT_FAILURE => DEPLOYING
+        - DEPLOYING => FLINK_STATES(RUNNING/FAILED/CANCELLED/CANCELLING)
+        - delete resource => CANCELLING
+        - delete done => CANCELLED
+    Currently, cancelled state is not recovered
     """
 
     namespace = body['metadata'].get("namespace")
@@ -76,13 +73,13 @@ def updates(beamsqltables: kopf.Index, stopped, patch, logger, body, spec, statu
     state = body['status'].get(STATE)
     logger.debug(f"Triggered updates for {namespace}/{name} with state {state}")
     
-    if state == States.INITIALIZED.name or state == States.SUBMISSION_FAILURE.name:
-        # submitting
-        logger.debug(f"Submitting {namespace}/{name}")
+    if state == States.INITIALIZED.name or state == States.DEPLOYMENT_FAILURE.name:
+        # deploying
+        logger.debug(f"Deyploying {namespace}/{name}")
         
         # get first all table ddls
         # get inputTable and outputTable
-        ddls = ""
+        ddls = f"SET pipeline.name = '{namespace}/{name}';\n"
         try:
             table_names = spec.get("tables")
             for table_name in table_names:
@@ -102,20 +99,27 @@ def updates(beamsqltables: kopf.Index, stopped, patch, logger, body, spec, statu
             statementset += statement + "\n"
             # TODO: check for "insert into" prefix and terminating ";" in sqlstatement
         statementset += "END;"
-        logger.debug(f"Now submitting statementset {statementset}")
+        logger.debug(f"Now deploying statementset {statementset}")
         try:
-            job_id = submit_statementset(statementset, logger)
+            job_id = deploy_statementset(statementset, logger)
         except Exception as err:
-            # submission failed
+            # deploying failed
             # Temporary error as we do not know the reason
-            patch.status[STATE] = States.SUBMISSION_FAILURE.name
+            patch.status[STATE] = States.DEPLOYMENT_FAILURE.name
             patch.status[JOB_ID] = None
-            logger.error(f"Could not submit statementset {err}")
-            raise kopf.TemporaryError(f"Could not submit statement: {err}", timer_backoff_temporary_failure_seconds)
+            logger.error(f"Could not deploy statementset {err}")
+            raise kopf.TemporaryError(f"Could not deploy statement: {err}", timer_backoff_temporary_failure_seconds)
         
-        patch.status[STATE] = States.SUBMITTED.name
+        patch.status[STATE] = States.DEPLOYING.name
         patch.status[JOB_ID] = job_id
-            
+        return
+    
+    # If state is not INITIALIZED, DEPLOYMENT_FAILURE nor DELETED, the state is monitored
+    elif not state == States.DELETED.name:
+        state = body['status'].get(STATE)
+        job_id = body['status'].get(JOB_ID)
+        flink_util.get_job_status(logger, job_id)
+
 
 
 def create_ddl_from_beamsqltables(body, beamsqltable, logger):
@@ -185,11 +189,11 @@ def create_ddl_from_beamsqltables(body, beamsqltable, logger):
     return ddl
     
 
-def submit_statementset(statementset, logger):
+def deploy_statementset(statementset, logger):
     """
-    submit statementset to flink SQL gateway
+    deploy statementset to flink SQL gateway
 
-    A statementset is submitted to the flink sql gateway. It is expected that all statements
+    A statementset is deployed to the flink sql gateway. It is expected that all statements
     in a set are inserting the result into a table.
     The table schemas are defined at the beginning
 
@@ -199,17 +203,17 @@ def submit_statementset(statementset, logger):
         The fully defined statementset including table ddl
     logger: obj
         logger obj
-    returns: jobid: string or exception if submission was not successful
+    returns: jobid: string or exception if deployment was not successful
         id of the deployed job
         
     """
 
     request = f"{FLINK_SQL_GATEWAY}/v1/sessions/session/statements"
-    logger.debug(f"Submitting request to SQL Gateway {request}")
+    logger.debug(f"Deployment request to SQL Gateway {request}")
     response = requests.post(request,
                              json={"statement": statementset})
     if response.status_code != 200:
-        raise Exception(f"Could not submit job to {request}, server returned: {response.status_code}")
+        raise Exception(f"Could not deploy job to {request}, server returned: {response.status_code}")
     logger.debug(f"Response: {response.json()}")
     job_id = response.json().get("jobid")
     return job_id
---
+++ services-operator/flink_util.py
@@ -0,0 +1,14 @@
+import os
+import re
+import requests
+
+FLINK_URL = os.getenv("OISP_FLINK_REST") or f"http://flink-jobmanager-rest.{namespace}:8081"
+
+
+def get_job_status(logger, job_id):
+    logger.debug(f"Requestion status for {job_id} from flink job-manager")
+    job_request = requests.get(
+        f"{FLINK_URL}/jobs/{element['id']}").json()
+    logger.debug(f"Received job status: {job_request}")
+    return job_request
+
